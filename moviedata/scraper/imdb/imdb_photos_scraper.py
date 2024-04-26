e8b3a7229c67b0dba9749341ca3422ae14e2d536
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-27 00:48:19 +0530

    Update imdb_photos_scraper.py

--- moviedata/scraper/imdb/imdb_photos_scraper.py
+++ moviedata/scraper/imdb/imdb_photos_scraper.py
@@ -1,5 +1,7 @@
 from moviedata import *
 from moviedata.scraper.imdb.imdb_scraper import *
 
+
 class IMDBPhotosScraper(IMDBScraper):
-    pass
+    def photos_details(self, movie_id, photo_url):
+        pass
