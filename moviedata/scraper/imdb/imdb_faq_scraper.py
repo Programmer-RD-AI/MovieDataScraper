74cbf47e141e872c3851483311218d9c407d1f40
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-27 00:48:16 +0530

    Update imdb_faq_scraper.py

--- moviedata/scraper/imdb/imdb_faq_scraper.py
+++ moviedata/scraper/imdb/imdb_faq_scraper.py
@@ -1,5 +1,7 @@
 from moviedata import *
 from moviedata.scraper.imdb.imdb_scraper import *
 
+
 class IMDBFAQScraper(IMDBScraper):
-    pass
+    def faq_details(self, movie_id, faq_url):
+        pass
