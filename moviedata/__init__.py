af49494be56e87e4f7553b83c0498b7efe691380
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-26 18:11:16 +0530

    Update __init__.py

--- moviedata/__init__.py
+++ moviedata/__init__.py
@@ -1,15 +1,10 @@
-
-import numpy as np
-import json
 from tqdm import tqdm
 import pandas as pd
 import os
 from dotenv import load_dotenv
-
-try:
-    from urllib.request import urlretrieve
-except ImportError:
-    from urllib import urlretrieve
+from pprint import pprint
+import threading
+from typing import *
 from bs4 import *
 import requests
 
@@ -17,4 +12,4 @@ load_dotenv()
 
 from moviedata.helper_functions import *
 from moviedata.analysis import *
-from moviedata.scraping import *
+from moviedata.scraper import *
