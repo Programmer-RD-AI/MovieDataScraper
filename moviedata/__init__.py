3672a2d40144d1a477807316eb7b461975563cda
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-29 22:51:06 +0530

    Update __init__.py

--- moviedata/__init__.py
+++ moviedata/__init__.py
@@ -1,4 +1,3 @@
-
 from tqdm import tqdm
 import pandas as pd
 import os
@@ -8,11 +7,19 @@ import threading
 from typing import *
 from bs4 import *
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 load_dotenv()
-PATH = os.getenv('PATH_OF_DIR')
+PATH = os.getenv("PATH_OF_DIR")
+retry_strategy = Retry(total=10, backoff_factor=5)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = requests.Session()
+http.mount("https://", adapter)
+http.mount("http://", adapter)
 
 from moviedata.helper_functions import *
 from moviedata.scraper import *
 from moviedata.analysis import *
-
